2fceffe5c5426dab1d076b3ccb0e2a508c9757ef
WellingtonLSoares <wellingtonlima1344@gmail.com>
2025-12-17 18:17:36 -0300

    feat: implementa os serviços de editar e deletar US.

--- backend/app/services/user_story_service.py
+++ backend/app/services/user_story_service.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session, load_only
 from app.models.user_story import UserStory
 from app.schemas.user_story_schema import UserStoryCreate
 from fastapi import HTTPException, status
+from sqlalchemy import and_
 
 def create_user_story(db: Session, story_data: UserStoryCreate, current_user_id: int):
   db_story = db.query(UserStory).filter(UserStory.title == story_data.title).options(
@@ -32,7 +33,7 @@ def create_user_story(db: Session, story_data: UserStoryCreate, current_user_id:
 
 def get_stories_paginated(db: Session, skip: int = 0, limit: int = 10):
   """
-  Retorna as histórias de forma paginada para popular a tabela do Figma.
+  Retorna as histórias de forma paginada.
   """
   query = db.query(UserStory)
   
@@ -45,3 +46,48 @@ def get_stories_paginated(db: Session, skip: int = 0, limit: int = 10):
     "skip": skip,
     "limit": limit
 }
+
+def update_user_story(db: Session, story_id: int, story_data: UserStoryCreate):
+  """
+  Permite editar título e descrição.
+  Valida unicidade do título se for alterado.
+  """
+  db_story = db.query(UserStory).filter(UserStory.id == story_id).first()
+  
+  # add validacao de usuario? us nao menciona esse tipo de coisa, dando a entender acesso geral, bastando estar logado
+
+  if not db_story:
+    raise HTTPException(status_code=404, detail="História de usuário não encontrada.")
+
+  if story_data.title != db_story.title:
+    title_exists = db.query(UserStory).filter(
+      and_(UserStory.title == story_data.title, UserStory.id != story_id)
+    ).first()
+
+    if title_exists:
+      raise HTTPException(status_code=400, detail="O título já está em uso.")
+
+  db_story.title = story_data.title
+  db_story.description = story_data.description
+  db_story.parent_id = story_data.parent_id
+
+  db.commit()
+  db.refresh(db_story)
+
+  return db_story
+
+def delete_user_story(db: Session, story_id: int):
+  """
+  Remove a história do sistema.
+  """
+  db_story = db.query(UserStory).filter(UserStory.id == story_id).first()
+  
+  # add validacao de usuario? us nao menciona esse tipo de coisa, dando a entender acesso geral, bastando estar logado
+
+  if not db_story:
+    raise HTTPException(status_code=404, detail="História de usuário não encontrada.")
+
+  db.delete(db_story)
+  db.commit()
+
+  return {"message": "História excluída com sucesso."}
