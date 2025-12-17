ecacc8ed1720f5919cf84ab78c36e55ca0f561a2
WellingtonLSoares <wellingtonlima1344@gmail.com>
2025-12-16 23:54:04 -0300

    chore: configura alembic para gerenciar migrations

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
-# check_same_thread=False é necessário apenas para SQLite
 engine = create_engine(
   SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
@@ -11,10 +10,9 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependência para injetar o banco nas rotas
 def get_db():
   db = SessionLocal()
-  
+
   try:
     yield db
   finally:
