74e8fd15842bd6b43736088c2b20549d9dac80b6
WellingtonLSoares <wellingtonlima1344@gmail.com>
2025-12-17 18:18:02 -0300

    feat: cria as rotas de editar e deletar US

--- backend/app/routers/user_story_route.py
+++ backend/app/routers/user_story_route.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from app.database import get_db
 from app.schemas.user_story_schema import UserStoryCreate, UserStoryResponse, UserStoryPaginatedResponse
+from app.schemas.generic_schema import MessageResponse
 from app.services import user_story_service
 from app.dependencies import get_current_user
 from app.models.users import User
@@ -29,3 +30,27 @@ def list_stories(
   🔒 Exige Autenticação.
   """
   return user_story_service.get_stories_paginated(db, skip=skip, limit=limit)
+
+@router.put("/{story_id}", response_model=UserStoryResponse)
+def update_story(
+  story_id: int, 
+  story: UserStoryCreate, 
+  db: Session = Depends(get_db),
+  current_user: User = Depends(get_current_user)
+):
+  """
+  Edita uma história existente.
+  """
+  return user_story_service.update_user_story(db, story_id, story)
+
+# EXCLUIR
+@router.delete("/{story_id}", response_model=MessageResponse)
+def delete_story(
+  story_id: int, 
+  db: Session = Depends(get_db),
+  current_user: User = Depends(get_current_user)
+):
+  """
+  Exclui permanentemente uma história de usuário.
+  """
+  return user_story_service.delete_user_story(db, story_id)
