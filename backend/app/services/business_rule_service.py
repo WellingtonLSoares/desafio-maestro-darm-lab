114941ae8c70190608bcff4842edadc642fa4fb3
WellingtonLSoares <wellingtonlima1344@gmail.com>
2025-12-17 22:08:28 -0300

    feat: implementa função para atualizar regras de negócio

--- backend/app/services/business_rule_service.py
+++ backend/app/services/business_rule_service.py
@@ -31,3 +31,22 @@ def get_rns_paginated(db: Session, skip: int = 0, limit: int = 5):
     "skip": skip, 
     "limit": limit
   }
+
+def update_rn(db: Session, rn_id: int, data: BusinessRuleCreate):
+  # TODO criar uma classe base para rn e user story, a logica eh msm, inclusive os dados
+  db_rn = db.query(BusinessRule).filter(BusinessRule.id == rn_id).first()
+  
+  if not db_rn:
+    raise HTTPException(status_code=404, detail="Regra de Negócio não encontrada")
+  
+  if data.title != db_rn.title:
+    if db.query(BusinessRule).filter(BusinessRule.title == data.title).first():
+      raise HTTPException(status_code=400, detail="Desculpe, o titulo já está em uso")
+
+  db_rn.title = data.title
+  db_rn.description = data.description
+  
+  db.commit()
+  db.refresh(db_rn)
+
+  return db_rn
